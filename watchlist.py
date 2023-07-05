141f9aebf19b3cc94f8d9714942352e16a3b8b6e
Naman Jain <naman.jain.ug20@nsut.ac.in>
2023-07-05 13:33:15 +0530

    Initial Commit

---
+++ watchlist.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+class watchlist:
+    def __init__(self):
+        self.movies_list = list()
+        pass
+
+    def add(self, movie_name):
+        self.movies_list.append(movie_name)
+
+    def remove(self, movie_name):
+        self.movies_list.remove(movie_name)
